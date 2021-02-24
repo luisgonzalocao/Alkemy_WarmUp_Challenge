2194e5a7694929d68eb9f7227ffc7177889bfddd
Luis Gonzalo Cao <luisgonzalocao@gmail.com>
2021-02-24 18:21:07 -0300

    feat: Services Post

---
+++ Challenge/Backend/services/post.service.js
@@ -0,0 +1,11 @@
+const BaseService = require("./base.service");
+
+class PostService extends BaseService {
+  constructor({ PostBusiness }) {
+    super(PostBusiness);
+  }
+  
+  //
+}
+
+module.exports = PostService;
